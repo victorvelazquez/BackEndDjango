ca5628fe43274b3971519d07b723e63ff63bd979
VictorVelazquez <victorvelazqueza@gmail.com>
2015-02-16 02:20:44 -0300

    Vista enlazada con una url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^personas/', 'personas_views.persona_view', name='persona_view'),
 )
