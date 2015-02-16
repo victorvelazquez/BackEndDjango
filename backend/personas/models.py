f045e5196f12d0aa053433152218ab81ae4bddf4
VictorVelazquez <victorvelazqueza@gmail.com>
2015-02-16 01:53:40 -0300

    Agregado atributos al modelo Personas

--- backend/personas/models.py
+++ backend/personas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Persona(models.Model):
+	nombre=models.CharField(max_length=255)
+	apodo=models.CharField(max_length=255, blank=True)
+	foto=models.ImageField(upload_to='personas')
+	limiteCredito=models.PositiveIntegerField()
+	obs=models.TextField(blank=True)
+
+	def __unicode__(self):
+		return self.nombre
